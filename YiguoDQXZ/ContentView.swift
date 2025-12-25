46ef32c9878a4afa7f90436b552da10e651d5598
赵燕燕 <zhaoyanyan2027@icloud.com>
2025-12-25 17:01:13 +0800

    Initial Commit

---
+++ YiguoDQXZ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YiguoDQXZ
+//
+//  Created by 赵燕燕 on 2025/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YiguoDQXZ/YiguoDQXZApp.swift
@@ -0,0 +1,17 @@
+//
+//  YiguoDQXZApp.swift
+//  YiguoDQXZ
+//
+//  Created by 赵燕燕 on 2025/12/25.
+//
+
+import SwiftUI
+
+@main
+struct YiguoDQXZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
